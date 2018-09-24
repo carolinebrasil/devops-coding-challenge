5ac492ae0f9a5e938465137dbb7cfee5bf329714
Nicolas Tremblier <ntremblier@gmail.com>
2018-09-24 12:53:10 +0200

    Finalisation de la version 1.0

--- README.md
+++ README.md
@@ -1,33 +1,34 @@
 # Coveo DevOps Challenge
-### The Challenge
-Your challenge, should you choose to accept it, is to develop an AWS S3 storage analysis tool. To test your tool, you will have to create a free [Amazon](http://aws.amazon.com/en/free/) account (if you don't already have one).
-### Specifications
-The tool is a shell command line utility (could be either Windows, Mac or Linux) that returns informations over all [S3](https://aws.amazon.com/documentation/s3/) buckets in an Amazon account.
-##### The tool must returns the following informations:
-- Bucket name
-- Creation date (of the bucket)
-- Number of files
-- Total size of files
-- Last modified date (most recent file of a bucket)
+### Le défi
+Ton défi, si tu l’acceptes, est de développer un outil permettant d’analyser la taille des ressources de stockage S3 d’un compte Amazon Web Services (AWS).
+Afin de tester ton outil, tu peux te créer un compte gratuit sur [Amazon](http://aws.amazon.com/fr/free/) (si tu n’en as pas déjà un).
+### Spécifications
+L’outil doit se présenter sous forme d’une commande shell (Windows, Mac ou Linux) qui permet d’obtenir des informations sur l’ensemble des ressources [S3](https://aws.amazon.com/documentation/s3/) d’un compte Amazon.
+##### L’outil doit permettre d’obtenir les informations suivantes:
+- Nom du bucket
+- Date de création
+- Nombre de fichiers
+- Taille totale des fichiers
+- Dernière date de mise-à-jour
 
-##### The following options should be supported:
-- Ability to get the size results in bytes, KB, MB, ...
-- Organize the information by [storage type](https://docs.aws.amazon.com/AmazonS3/latest/dev/storage-class-intro.html) (Standard, IA, RR)
-- Filter the results in a list of buckets (bonus point for regex support)
-- Ability to group information by [regions](http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html)
+##### Les options suivantes doivent être supportées:
+- Possibilité de sortir les résultats en octets, Kilooctets, Megaoctets, … ;
+- Pouvoir sortir les informations par [type de stockage](https://docs.aws.amazon.com/AmazonS3/latest/dev/storage-class-intro.html) (Standard, IA, RR) ;
+- Pouvoir spécifier une liste de buckets (bonus si support des expressions régulières) ;
+- Pouvoir regrouper les informations par [régions](http://docs.aws.amazon.com/fr_fr/AWSEC2/latest/UserGuide/using-regions-availability-zones.html).
 
-##### Some additional features that could be useful (optional)
-It would be nice to support prefix in the bucket filter (e.g.: s3://mybucket/Folder/SubFolder/log*). It may also be useful to organize the results according to the [encryption type](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingEncryption.html), get additional buckets informations (life cycle, cross-region replication, etc.) or take into account the [previous file versions](https://docs.aws.amazon.com/AmazonS3/latest/UG/enable-bucket-versioning.html) in the count + size calculation.
+##### Idées de fonctionnalités supplémentaires
+Il serait bien de pouvoir filtrer les fichiers considérés dans le calcul à l’aide d’un préfixe (ex: s3://mybucket/Folder/SubFolder/log*). Il est également utile de pouvoir filtrer ou organiser les résultats selon le [type d'encryption](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingEncryption.html), d’obtenir des informations supplémentaires sur les buckets (Life cycle, cross-region replication, etc.) ou de tenir compte des [versions précédentes](https://docs.aws.amazon.com/AmazonS3/latest/UG/enable-bucket-versioning.html) des fichiers (nombre + taille).
 
-Some statistics to check the percentage of space used by a bucket, or any other good ideas you could have, are more than welcome.
-### Rules
-- Your are free to use the programming language and the [SDK](https://aws.amazon.com/tools/) of your choice.
-- We will test your work over our environment (which contains millions of files). The overall performance of your tool will be evaluated.
-- Your code must be made available as a git fork of our challenge or any other public version control software.  
+Des statistiques pour afficher le pourcentage de l’espace total occupé par un bucket ou toute autre bonne idée que tu pourrais avoir sont également les bienvenues.
+### Plus d'informations
+- Tu es libre d’utiliser le langage et le [SDK](https://aws.amazon.com/tools/) de ton choix ;
+- Nous allons tester le fruit de ton travail sur notre environnement qui, soit dit en passant, contient des millions de fichiers. La performance globale de la solution proposée est donc à considérer ;
+- Ton code doit être disponible à partir de n'importe quel gestionnaire de code source publique (tu peux faire un "fork" de notre challenge si tu veux).
 
-## Advices
+## Conseils
 
-- **Try to design and implement your solution as you would do for real production code**. Show us how you create clean, maintainable code that does awesome stuff. Build something that we'd be happy to contribute to. This is not a programming contest where dirty hacks win the game.
-- Feel free to add more features! Really, we're curious about what you can think of. We'd expect the same if you worked with us.
-- Documentation and maintainability is a plus.
-- Don't you forget those unit tests.
+- **Tente de créer ta solution comme si c'était du vrai code de production**. Montre nous comment tu crées du code propre et maintenable qui fait des choses incroyables. Construit quelque chose à laquelle nous serions heureux de contribuer. Ceci n'est pas un concours de programmation où les "hack" malpropres remportent la victoire.
+- N'hésite pas à ajouter des fonctionnalités! Nous sommes curieux de voir ce à quoi tu peux penser. Nous nous attendrons à la même chose si tu travailles avec nous.
+- La documentation et la maintenabilité est un plus.
+- N'oublie pas les tests unitaires.
--- bucket-stats.py
+++ bucket-stats.py
@@ -1,44 +1,189 @@
-#!/usr/local/bin/python3
-import boto3
-import datetime
+#!python3
 
-s3 = boto3.resource('s3')
-cloudwatch = boto3.client('cloudwatch')
+# ---------------------------------------
+# Bucket-stats est un script python permettant d'extraire des statistiques sur les buckets S3. 
+# Il se base en partie sur les metriques de la supervision cloudwatch dans le but d'optimiser les performances.
+# Ecrit par Nicolas Tremblier
+# v1.0 - 2018-09-23
+# ---------------------------------------
 
-now = datetime.datetime.now()
-for bucket in s3.buckets.all():
-	bucket_size = None
-	bucket_count = None
-	cloudwatch_size = cloudwatch.get_metric_statistics(
-		Namespace='AWS/S3',
-		MetricName='BucketSizeBytes',
-		Dimensions=[
-			{'Name': 'BucketName', 'Value': bucket.name},
-			{'Name': 'StorageType', 'Value': 'StandardStorage'}
-		],
-		Statistics=['Maximum'],
-		Period=86400,
-		StartTime=(now - datetime.timedelta(days=1)).isoformat(),
-		EndTime=now.isoformat()
-	)
+import boto3, datetime, sys, getopt, re
+from operator import itemgetter
+from table_logger import TableLogger
 
-	cloudwatch_count = cloudwatch.get_metric_statistics(
-		Namespace='AWS/S3',
-		MetricName='NumberOfObjects',
-		Dimensions=[
-			{'Name': 'BucketName', 'Value': bucket.name},
-			{'Name': 'StorageType', 'Value': 'AllStorageTypes'}
-		],
-		Statistics=['Maximum'],
-		Period=86400,
-		StartTime=(now - datetime.timedelta(days=1)).isoformat(),
-		EndTime=now.isoformat()
-	)
+class buck: # Définition de la classe pour le bucket
 
-	if cloudwatch_size["Datapoints"]:
-		bucket_size = cloudwatch_size["Datapoints"][0]['Maximum']
+	# prix au 22/09 sur la console AWS -> https://aws.amazon.com/fr/pricing/?nc2=h_ql_pr
+	# Amélioration possible :  https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonS3/current/index.json
+	TARIFAWS = 0.023
 
-	if cloudwatch_count["Datapoints"]:
-		bucket_count = cloudwatch_count["Datapoints"][0]['Maximum']
+	compteur = 0 # compte le nombre de buckets créés
 
-	print('{0} \t\t {1} \t {2} \t {3}'.format(bucket.name, bucket.creation_date, bucket_size, bucket_count))
+	def __init__(self, s3bucket): # Constructeur
+		buck.compteur = buck.compteur + 1
+		self.nom = s3bucket.name 
+		self.datecreation = s3bucket.creation_date
+		self.taille = self.metriqueCloudwatch(s3bucket,"BucketSizeBytes", "StandardStorage") # appel à cloudwatch pour récupérer les stats
+		self.nbreObj = self.metriqueCloudwatch(s3bucket,"NumberOfObjects", "AllStorageTypes") # appel à cloudwatch pour récupérer les stats
+		
+		try: # Récupération de la stratégie de chiffrement, sinon, non chiffré
+			boto3.client('s3').get_bucket_encryption(Bucket=s3bucket.name)
+			self.chiffre = True
+		except:
+			self.chiffre = False
+		
+		# Récupère les infos sur la région du bucket
+		self.region = (boto3.client('s3').get_bucket_location(Bucket=s3bucket.name))['LocationConstraint'] # Récupère la région du bucket
+		
+		# calcul du cout du bucket, source en octets
+		self.cout = round(self.taille / 1024**3 * self.TARIFAWS,2) 
+				
+		try: # Récupération de la stategie de réplication, sinon, non répliqué
+			boto3.client('s3').get_bucket_replication(Bucket=s3bucket.name)
+			self.replique = True
+		except:
+			self.replique = False
+
+		# - Last modified date (most recent file of a bucket) / nécessite de passer tous les fichiers en revue
+		def collObjInfo(self):
+			s3obj = (boto3.client('s3')).list_objects_v2(Bucket=self.nom)
+			
+			if s3obj['KeyCount'] != 0:
+				self.derniereMAJ = s3obj['Contents'][0]['LastModified']
+				self.typeStockage = s3obj['Contents'][0]['StorageClass']
+			 
+		collObjInfo(self)
+		self.publique = False # Récupère si le bucket est publié
+
+
+	def __str__(self): #permet d'afficher le contenu de l'object via print
+		return str(self.__class__) + ": " + str(self.__dict__)
+
+	def __getitem__(self, key): #permet de récupérer les champ pour la fonction sorted
+		if key == 'region':
+			return self.region
+		if key == 'typeStockage':
+			return self.typeStockage
+
+	def getSize(self, human=False): #retourne la taille en fonction de l'option utilisateur
+		if human:
+			return humanReadable(self.taille)
+		else:
+			return self.taille
+
+	def metriqueCloudwatch(self, bucket, NomMetrique, stockage): 	# Méthode de collecte d'information dans cloudwatch
+		cloudwatch = boto3.client('cloudwatch')
+		now = datetime.datetime.now()
+		try:
+			cloudwatch_size = cloudwatch.get_metric_statistics(
+				Namespace='AWS/S3',
+				MetricName=NomMetrique,
+				Dimensions=[
+					{'Name': 'BucketName', 'Value': bucket.name},
+					{'Name': 'StorageType', 'Value': stockage}
+				],
+				Statistics=['Maximum'],
+				Period=86400,
+				StartTime=(now - datetime.timedelta(days=1)).isoformat(),
+				EndTime=now.isoformat()
+			)
+			if cloudwatch_size["Datapoints"]:
+				return cloudwatch_size["Datapoints"][0]['Maximum']
+			else:
+				return 0
+		except:
+			return 0
+		
+def humanReadable(num, suffix='B'): # permet de convertir les octets en valeurs lisibles, à la façon de df
+    for unit in ['','K','M','G','T','P']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+def aide(): #Affiche l'aide en cas de mauvais paramètre ou à la demande
+	print("Utilisation : bucket-stat [OPTIONS]")
+	print("Affiche des renseignements sur les buckets AWS S3, par defaut")
+	print("Arguments : \n\
+	--help \t\t\t affiche cette aide.\n\
+	--crypted-only \t\t n'affiche que les buckets chiffrés\n\
+	-c, --csv \t\t ffiche le résultat en CSV\n\
+	-s, --sorted \t\t groupe les résultats par region et groupe de stockage\n\
+ 	-h, --human-readable \t afficher les tailles dans des puissances de 1024 (par exemple 1023M)\n\
+	-f, --filter=FILTRE \t filtre la liste des buckets sur la base de l'expression regulière FILTRE") 
+			
+
+# ---------------------------------------
+# Partie principale du script
+# ---------------------------------------
+
+def main():
+	
+	csv=False
+	human = False
+	grouper = False
+	filtreCrpt = False
+	filtre = None
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "shcf:", ["sorted", "help", "csv", "human-readable", "crypted-only", "filter:"])
+	except:
+		# print help information and exit:
+		print("Commande incorrecte, voici l'aide : ")
+		aide()
+		sys.exit(2)
+
+	for opts, args in opts:
+		if opts == "--help":
+			aide()
+			sys.exit()
+		elif opts == "--crypted-only":
+			filtreCrpt = True
+		elif opts in ("-c", "--csv"):
+			csv = True
+		elif opts in ("-s", "--sorted"):
+			grouper = True
+		elif opts in ("-h", "--human-readable"):
+			human = True
+		elif opts in ("-f", "--filter"):
+			if len(args):
+				filtre = args
+			else:
+				aide()
+				sys.exit(2)
+		
+	s3 = boto3.resource('s3')
+	bucks = [] # Tableau d'objet de tous les buckets de l'organisation
+
+	listeS3Bucks = s3.buckets.all()
+	for bucket in listeS3Bucks:
+		try:
+			if filtre:
+				re.match(filtre,"Chaine de test")
+		except:
+			print("Erreur d'expression régulière")
+			sys.exit(2)
+
+		if (filtre and re.match(filtre,bucket.name)) or not filtre:
+			try:
+				bucks.append(buck(bucket)) # Création de l'objet et insertion dans le tableau
+			except:
+				print("Erreur lors de la connexion à AWS, merci de vérifier votre parametrage")
+				print("Pour plus d'information : https://docs.aws.amazon.com/cli/latest/userguide/cli-config-files.html")
+				sys.exit(2)
+
+	if grouper: #si l'option de grouper  activée, on reclasse le tableau par région et par type de stockage
+		bucks = sorted(bucks, key=itemgetter('region'))
+		bucks = sorted(bucks, key=itemgetter('typeStockage'))
+		
+	# Utilisation de Table-Logger pour l'affichage : https://github.com/AleksTk/table-logger/tree/master/table_logger
+	tbl = TableLogger(columns='Nom,Date de Création,Dernière MaJ,Taille,Nombre d\'objets,Chiffré,Stockage,Publique,Région,Cout,Répliqué',
+		csv=csv, border=False) 
+		
+	for cBuck in bucks:
+		if (filtreCrpt and cBuck.chiffre) or not filtreCrpt:
+			tbl(cBuck.nom, cBuck.datecreation, cBuck.derniereMAJ, cBuck.getSize(human), str(cBuck.nbreObj), 
+			cBuck.chiffre,cBuck.typeStockage, cBuck.publique, cBuck.region, "$"+str(cBuck.cout),cBuck.replique)
+		
+if __name__ == "__main__":
+	main()
